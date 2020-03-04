4eb724b300a2fcc40f0d033de53f7327c96bda4e
Eldorbek Makhkamov <52928263+makhweb@users.noreply.github.com>
2020-03-04 19:34:23 +0500

    Demo is finished ✔

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+@extends(fromLayout('layout'))
+
+@section('content')
+<!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/home-bg.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="site-heading">
+            <h1>Clean Blog</h1>
+            <span class="subheading">A Blog Theme by Start Bootstrap</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Main Content -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-10 mx-auto">
+        <h2>We haven't any posts :(</h2>
+        <hr>
+      </div>
+    </div>
+  </div>
+
+  <hr>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'home')->name('home');
+Route::view('/about', 'about')->name('about');
+Route::view('/post', 'post')->name('post');
+Route::view('/contact', 'contact')->name('contact');
